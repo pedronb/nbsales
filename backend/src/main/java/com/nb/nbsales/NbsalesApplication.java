4aba160910e8e6056fc5d37c7dcee96fccdac4ef
Pedro Neves <pedronevesbezerra@gmail.com>
2022-07-12 11:29:01 -0300

    Project created

---
+++ backend/src/main/java/com/nb/nbsales/NbsalesApplication.java
@@ -0,0 +1,13 @@
+package com.nb.nbsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbsalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbsalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nb/nbsales/NbsalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.nbsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbsalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
