ccc84bb44116d4d902ca3135cbe9b5e69a57218b
Pedro Neves <pedronevesbezerra@gmail.com>
2022-07-27 12:48:38 -0300

    heroku

--- backend/src/main/java/com/nb/nbsales/controller/SaleController.java
+++ backend/src/main/java/com/nb/nbsales/controller/SaleController.java
@@ -22,5 +22,7 @@ public class SaleController {
     public Page<Sale> findSales(@RequestParam(value="minDate", defaultValue="") String minDate, @RequestParam(value="maxDate", defaultValue="") String maxDate, Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
     }
+
+   
     
 }
